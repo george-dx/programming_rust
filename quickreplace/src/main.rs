c0ae5d1f5a62a25c605c2ba0da4620f43ac5e500
George Daian <georgedaianx@gmail.com>
2024-02-07 15:40:58 +0200

    Quickreplace initial version - print usage functionality

---
+++ quickreplace/src/main.rs
@@ -0,0 +1,18 @@
+use text_colorizer::*;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
+fn print_usage() {
+    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn main() {
+    println!("Hello, world!");
+}

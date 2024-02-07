2b20477bd96d3ec3f2befa997140ea39b94509de
George Daian <georgedaianx@gmail.com>
2024-02-07 15:55:39 +0200

    Add find/replace capability

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::{env, fs};
 use text_colorizer::*;
 
@@ -38,6 +39,11 @@ fn parse_args() -> Arguments {
     }
 }
 
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
 
@@ -54,7 +60,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(

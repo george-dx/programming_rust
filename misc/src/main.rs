969a2a7a703bbcbfd2f31d2195a9005607be417f
George Daian <georgedaianx@gmail.com>
2024-02-13 16:37:29 +0200

    Use ? operator for propagation

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
-use std::io::{Write, stderr};
+use std::fs;
+use std::io;
+use std::io::{stderr, Write};
+use std::path::Path;
 
 ///Dump an error message to `stderr`.
-/// 
+///
 /// If another error happens while building the error message
 /// or writing to `stderr`, it is ignored.
 
@@ -14,6 +17,14 @@ fn _print_error(mut err: &dyn Error) {
     }
 }
 
-fn main() {
-
+fn _move_all(src: &Path, dst: &Path) -> io::Result<()> {
+    for entry_result in src.read_dir()? {
+        //opening dir could fail
+        let entry = entry_result?; // reading dir could fail
+        let dst_file = dst.join(entry.file_name());
+        fs::rename(entry.path(), dst_file)?; // renaming could fain
+    }
+    Ok(())
 }
+
+fn main() {}

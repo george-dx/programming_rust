37db33fbcad69392af080905945cff5ee789b0ce
George Daian <georgedaianx@gmail.com>
2024-02-17 13:43:48 +0200

    Add implemnetation for binary tree element add

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -11,6 +11,27 @@ struct TreeNode<T> {
     right: BinaryTree<T>,
 }
 
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }))
+            }
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let jupiter_tree = NonEmpty(Box::new(TreeNode {
         element: "Jupiter",

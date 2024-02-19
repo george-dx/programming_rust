4fae473851bcf9b9e80adbc23171df3344d3b044
George Daian <georgedaianx@gmail.com>
2024-02-19 18:39:49 +0200

    Create trait for Visible object in game

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -1,8 +1,31 @@
 use std::error::Error;
 use std::fs;
-use std::io::{self, stderr, Write, BufRead};
+use std::io::{self, stderr, BufRead, Write};
 use std::path::Path;
 
+/// A trait for cahracters, items and scenery - anything in
+/// the game world that's visible on scree
+trait Visible {
+    /// Render this object on the given canvas
+    fn draw(&self, canvas: &mut Canvas);
+
+    /// Return true if clicking at (x, y) should
+    /// select this object
+    fn hit_test(&self, x: i32, y: i32) -> bool;
+}
+
+impl Visible for Broom {
+    fn draw(&self, canvas: &mut Canvas) {
+        for y in self.y - self.height - 1..self.y {
+            canvas.write_at(self.x, y, '|');
+        }
+        canvas.write_at(self.x, self.y, 'M');
+    }
+
+    fn hit_test(&self, x: i32, y: i32) -> bool {
+        self.x == x && self.y - self.height - 1 <= y && y <= self.y
+    }
+}
 
 type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
 type GenericResult<T> = Result<T, GenericError>;
@@ -33,8 +56,8 @@ fn _move_all(src: &Path, dst: &Path) -> io::Result<()> {
 fn _read_numbers(file: &mut dyn BufRead) -> GenericResult<Vec<i64>> {
     let mut numbers = vec![];
     for line_result in file.lines() {
-        let line = line_result?;        // reading lines can fail
-        numbers.push(line.parse()?);    // parsing integers can fail
+        let line = line_result?; // reading lines can fail
+        numbers.push(line.parse()?); // parsing integers can fail
     }
     Ok(numbers)
 }

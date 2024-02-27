93e22ab43f6cc8936338d3fe00965e0efb33841c
George Daian <georgedaianx@gmail.com>
2024-02-27 14:25:47 +0200

    Sort cities with closure

--- misc2/src/main.rs
+++ misc2/src/main.rs
@@ -12,6 +12,9 @@ fn sort_cities(cities: &mut Vec<City>) {
     cities.sort_by_key(city_population_descending);
 }
 
+fn sort_cities_with_closure(cities: &mut Vec<City>) {
+    cities.sort_by_key(|city| -city.population);
+}
 
 fn main() {
     println!("Hello, world!");

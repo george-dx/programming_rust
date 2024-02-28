83d2054df991768b0941b410632cbde48fec51a2
George Daian <georgedaianx@gmail.com>
2024-02-28 15:41:58 +0200

    Implement fnpointer router

--- router/src/main.rs
+++ router/src/main.rs
@@ -33,6 +33,29 @@ impl BasicRouter {
     {
         self.routes.insert(url.to_string(), Box::new(callback));
     }
+
+    fn handle_request(&self, request: &Request) -> Response {
+        match self.routes.get(&request.url) {
+            None => not_found_response(),
+            Some(callback) => callback(request),
+        }
+    }
+}
+
+struct FnPointerRouter {
+    routes: HashMap<String, fn(&Request) -> Response>,
+}
+
+impl FnPointerRouter {
+    fn new() -> FnPointerRouter {
+        FnPointerRouter {
+            routes: HashMap::new(),
+        }
+    }
+
+    fn add_route(&mut self, url: &str, callback: fn(&Request) -> Response) {
+        self.routes.insert(url.to_string(), callback);
+    }
 }
 
 fn main() {

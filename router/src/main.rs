0d8c780a7fd90af18df786918b2828b2fefd0183
George Daian <georgedaianx@gmail.com>
2024-03-01 18:21:42 +0200

    Exclude iguanas

--- router/src/main.rs
+++ router/src/main.rs
@@ -86,6 +86,6 @@ fn main() {
     println!("{:?}", lengths);
 
     let text = " ponies \n giraffes\niguanas \nsquid".to_string();
-    let v: Vec<&str> = text.lines().map(str::trim).collect();
-    assert_eq!(v, ["ponies", "giraffes", "iguanas", "squid"]);
+    let v: Vec<&str> = text.lines().map(str::trim).filter(|s| *s != "iguanas").collect();
+    assert_eq!(v, ["ponies", "giraffes", "squid"]);
 }

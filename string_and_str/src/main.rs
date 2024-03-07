158995f9137d52f1e5f008817a5f10d1a22b2449
George Daian <georgedaianx@gmail.com>
2024-03-07 18:54:33 +0200

    Strings handling exploring

--- string_and_str/src/main.rs
+++ string_and_str/src/main.rs
@@ -29,4 +29,30 @@ fn main() {
         "## Elephants".trim_start_matches(|ch: char| ch == '#' || ch.is_whitespace()),
         "Elephants"
     );
+
+    assert!("2017".starts_with(char::is_numeric));
+    let quip = "We also know there are known unknowns";
+    assert_eq!(quip.find("know"), Some(8));
+    assert_eq!(quip.rfind("know"), Some(31));
+
+    assert_eq!(
+        "The only thing we have to fear is fear itself".replace("fear", "spin"),
+        "The only thing we have to spin is spin itself"
+    );
+
+    // replace is done eagerly
+    assert_eq!("cabababababbage".replace("aba", "***"), "c***b***babbage");
+
+    assert_eq!(
+        "jimb:100:Jim Blandy:".split(':').collect::<Vec<_>>(),
+        vec!["jimb", "100", "Jim Blandy", ""]
+    );
+
+    assert_eq!("\t*.rs    ".trim(), "*.rs");
+
+    assert_eq!(format!("{}, wow", "doge"), "doge, wow");
+    assert_eq!(format!("{}", true), "true");
+
+    let good_utf8: Vec<u8> = vec![0xe9, 0x8c, 0x86];
+    assert_eq!(String::from_utf8(good_utf8).ok(), Some("錆".to_string()));
 }

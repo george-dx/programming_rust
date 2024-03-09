197206ba5a40f6909dfc0a50d11bed85035d3682
George Daian <georgedaianx@gmail.com>
2024-03-09 12:47:26 +0200

    Extend grep to accept bufread and stdin read

--- input_output/src/main.rs
+++ input_output/src/main.rs
@@ -1,9 +1,14 @@
-use std::io;
+use std::fs::File;
+use std::io::{self, BufReader};
 use std::io::prelude::*;
+use std::error::Error;
+use std::path::PathBuf;
 
-fn grep(target: &str) -> io::Result<()> {
-    let stdin = io::stdin();
-    for line_result in stdin.lock().lines() {
+fn grep<R>(target: &str, reader: R) -> io::Result<()>
+where
+    R: BufRead,
+{
+    for line_result in reader.lines() {
         let line = line_result?;
         if line.contains(target) {
             println!("{}", line);
@@ -12,6 +17,31 @@ fn grep(target: &str) -> io::Result<()> {
     Ok(())
 }
 
+fn grep_main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args().skip(2);
+    println!("{:?}", args);
+    let target = match args.next() {
+        Some(s) => s,
+        None => Err("usage: grep PATTERN FILE...")?
+    };
+    let files: Vec<PathBuf> = args.map(PathBuf::from).collect();
+
+    if files.is_empty() {
+        let stdin = io::stdin();
+        grep(&target, stdin.lock());
+    } else {
+        for file in files {
+            let f = File::open(file)?;
+            grep(&target, BufReader::new(f))?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    grep("yolo");
+    let result = grep_main();
+    if let Err(err) = result {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    }
 }

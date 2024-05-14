aa8b80242c8dd9fc25e67bfc4f19274323b19e86
George Daian <georgedaianx@gmail.com>
2024-05-14 11:01:50 +0300

    Update cheap request to be async

--- async_programming/src/main.rs
+++ async_programming/src/main.rs
@@ -1,13 +1,16 @@
-use std::{net, thread};
+use async_std::io::prelude::*;
+use async_std::net;
+
+async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
+    let mut socket = net::TcpStream::connect((host, port)).await?;
+    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
+    socket.write_all(request.as_bytes()).await?;
+    socket.shutdown(net::Shutdown::Write)?;
+    let mut response = String::new();
+    socket.read_to_string(&mut response).await?;
+    Ok(response)
+}
 
 fn main() {
-    let listener = net::TcpListener::bind(address)?;
-
-    for socket_result in listener.incoming() {
-        let socker = socker_result?;
-        let groups = chat_group_table.clone();
-        thread::spawn(|| { 
-            log_error(serve(socker, groups));
-        });
-    }
+    println!("Hello");
 }

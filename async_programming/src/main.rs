949b4f837b30e79f2eee8d1411c914c495006899
George Daian <georgedaianx@gmail.com>
2024-05-15 18:46:20 +0300

    A real asynchronous http client

--- async_programming/src/main.rs
+++ async_programming/src/main.rs
@@ -15,29 +15,32 @@ async fn cheapo_owning_request(host: String, port: u16, path: String) -> std::io
     cheapo_request(&host, port, &path).await
 }
 
-pub async fn many_requests(requests: Vec<(String, u16, String)>) -> Vec<std::io::Result<String>> {
+pub async fn many_requests(urls: &[String]) -> Vec<Result<String, surf::Exception>> {
+    let client = surf::Client::new();
     let mut handles = vec![];
-    for (host, port, path) in requests {
-        handles.push(task::spawn_local(cheapo_owning_request(host, port, path)));
+    for url in urls {
+        let request = client.get(&url).recv_string();
+        handles.push(async_std::task::spawn(request));
     }
     let mut results = vec![];
     for handle in handles {
         results.push(handle.await);
     }
+
     results
 }
 
 fn main() -> std::io::Result<()> {
-    let requests = vec![
-        ("example.com".to_string(), 80, "/".to_string()),
-        ("www.red-bean.com".to_string(), 80, "/".to_string()),
-        ("en.wikipedia.org".to_string(), 80, "/".to_string()),
+    let requests = &[
+        "http://example.com".to_string(),
+        "https://www.red-bean.com".to_string(),
+        "https://en.wikipedia.org/wiki/Main_Page".to_string(),
     ];
     let results = async_std::task::block_on(many_requests(requests));
     for result in results {
         match result {
-            Ok(response) => println!("{}", response),
-            Err(err) => eprintln!("error: {}", err),
+            Ok(response) => println!("*** {}\n", response),
+            Err(err) => eprintln!("error: {}\n", err),
         }
     }
     Ok(())

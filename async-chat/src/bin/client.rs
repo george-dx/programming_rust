46f200bdeebaa669cafc81eac8de887c1c569cce
George Daian <georgedaianx@gmail.com>
2024-05-16 16:40:38 +0300

    Take user input

---
+++ async-chat/src/bin/client.rs
@@ -0,0 +1,62 @@
+use async_std::{prelude::*, io, net};
+use async_chat::utils::{self, ChatResult};
+use async_chat::FromClient;
+use std::sync::Arc;
+
+
+async fn send_commands(mut to_server: net::TcpStream) -> ChatResult<()> {
+    println!("Commands:\n\
+    join GROUP\n\
+    post GROUP MESSAGE...\n\
+    Type Control-D (on Unix) or Control-Z (on Windows) \
+    to close the connection.");
+
+    let mut command_lines = io::BufReader::new(io::stdin()).lines();
+    while let Some(command_result) = command_lines.next().await {
+        let command = command_result?;
+        let request = match parse_command(&command) {
+            Some(request) => request,
+            None => continue,
+        };
+
+        utils::send_as_json(&mut to_server, &request).await?;
+        to_server.flush().await?;
+    }
+    Ok(())
+}
+
+fn parse_command(line: &str) -> Option<FromClient> {
+    let (command, rest) = get_next_token(line)?;
+    if command == "post" {
+        let (group, rest) = get_next_token(rest)?;
+        let message = rest.trim_start().to_string();
+        return Some(FromClient::Post {
+            group_name: Arc::new(group.to_string()),
+            message: Arc::new(message),
+        });
+    } else if command == "join" {
+        let (group, rest) = get_next_token(rest)?;
+        if !rest.trim_start().is_empty() {
+            return None;
+        }
+        return Some(FromClient::Join {
+            group_name: Arc::new(group.to_string()),
+        });
+    } else {
+        eprintln!("Unrecognized command: {:?}", line);
+        return None;
+    }
+}
+
+fn get_next_token(mut input: &str) -> Option<(&str, &str)> {
+    input = input.trim_start();
+
+    if input.is_empty() {
+        return None;
+    }
+
+    match input.find(char::is_whitespace) {
+        Some(space) => Some((&input[0..space], &input[space..])),
+        None => Some((input, "")),
+    }
+}

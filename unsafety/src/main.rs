148cbe0e508ffd08cd9a273d1797d1dc8655f031
George Daian <georgedaianx@gmail.com>
2024-05-21 10:48:53 +0300

    Add GapBuffer base structure

--- unsafety/src/main.rs
+++ unsafety/src/main.rs
@@ -1,7 +1,55 @@
-fn main() {
-    let mut a: usize = 0;
-    let ptr = &mut a as *mut usize;
-    unsafe {
-        *ptr.offset(3) = 0x7ffff72f484c;
+use std;
+use std::ops::Range;
+
+pub struct GapBuffer<T> {
+    storage: Vec<T>,
+    gap: Range<usize>,
+}
+
+impl<T> GapBuffer<T> {
+    pub fn new() -> GapBuffer<T> {
+        GapBuffer {
+            storage: Vec::new(),
+            gap: 0..0,
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.storage.capacity()
+    }
+
+    pub fn len(&self) -> usize {
+        self.capacity() - self.gap.len()
+    }
+
+    pub fn position(&self) -> usize {
+        self.gap.start
+    }
+
+    unsafe fn space(&self, index: usize) -> *const T {
+        self.storage.as_ptr().offset(index as isize)
+    }
+
+    unsafe fn space_mut(&mut self, index: usize) -> *mut T {
+        self.storage.as_mut_ptr().offset(index as isize)
+    }
+
+    fn index_to_raw(&self, index: usize) -> usize {
+        if index < self.gap.start {
+            index
+        } else {
+            index + self.gap.len()
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        let raw = self.index_to_raw(index);
+        if raw < self.capacity() {
+            unsafe { Some(&*self.space(raw)) }
+        } else {
+            None
+        }
     }
 }
+
+fn main() {}
